1e08176d5158b02d16b8e12e1205c5704e0300d6
sojaekyung <sojaekyung@ownerclan.com>
2018-12-05 18:22:21 +0900

    개발하는 동안 타입체크 안하도록

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   output: {
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+      { 
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
     new ForkTsCheckerWebpackPlugin(),
   ],
-}
+};
