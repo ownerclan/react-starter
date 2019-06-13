ac421fa5920ac38b8e3d14ecf5f2a19a4a63c642
sojaekyung <sojaekyung@ownerclan.com>
2019-06-13 16:12:08 +0900

    no more class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-export default class extends React.Component<{world?: string}> {
-  public render() {
-    return <div className="app">Hello, {this.props.world}</div>;
-  }
+export default (props: {world?: string}) => {
+  return <div className="app">Hello, {props.world}</div>;
 }
